bd43dff98a8b8e6ac35f4dcc4773ec77b4633be9
SRodriguezIITH <sonitnitin@gmail.com>
2025-02-21 21:57:54 +0530

    fix

--- api/app.py
+++ api/app.py
@@ -7,8 +7,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-# CORS(app, supports_credentials=True)
 CORS(app, supports_credentials=True)
+# CORS(app, resources={r"/api/*": {"origins": "https://compute-x-tau.vercel.app"}})
 
 
 import torch
